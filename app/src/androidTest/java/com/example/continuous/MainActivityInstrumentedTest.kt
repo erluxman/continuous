5216aab57a01a9ad169863ddbb3d4ad78ced4836
Laxman Bhattarai <erluxman@gmail.com>
2019-04-30 12:07:07 +0545

    initial commit

---
+++ app/src/androidTest/java/com/example/continuous/MainActivityInstrumentedTest.kt
@@ -0,0 +1,41 @@
+package com.example.continuous
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest{
+    @get:Rule
+    val rule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun textStartsWithHi(){
+        Espresso.onView(ViewMatchers.withId(R.id.textView)).check(ViewAssertions.matches(ViewMatchers.withText("Hi")))
+    }
+
+    @Test
+    fun textChangesTo_Clicked_whenFabClicked(){
+        Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+            .check(ViewAssertions.matches(ViewMatchers.withText("Clicked")))
+
+    }
+
+    @Test
+    fun textChangesTo_LongClicked_whenFabLongClicked() {
+        Espresso.onView(ViewMatchers.withId(R.id.fab)).perform(ViewActions.longClick())
+        Espresso.onView(ViewMatchers.withId(R.id.textView))
+            .check(ViewAssertions.matches(ViewMatchers.withText("LongClicked")))
+
+    }
+
+}
---
+++ app/src/main/java/com/example/continuous/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.continuous
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { textView.text = "Clicked" }
+        fab.setOnLongClickListener {
+            textView.text = "LongClicked"
+            true
+        }
+    }
+}
